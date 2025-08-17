098a23ae1d6ea384041bbbf3b36c42e4be198330
ashugaur <ashutoshgaur@live.com>
2025-08-17 20:54:41 +0530

    add

--- src/analytics_tasks_utils/os_functions.py
+++ src/analytics_tasks_utils/os_functions.py
@@ -12,7 +12,7 @@ def open_file_folder(path):
     path_adj = "explorer " + '"' + str(path) + '"'
     subprocess.Popen(path_adj)
 
-
+## get_downloads_folder
 def get_downloads_folder():
     """
     Automatically runs to get the downloads folder directory.
@@ -126,3 +126,46 @@ def copy_multiple_files(source_files, destination):
             print(f"Error copying {source_file}: {str(e)}")
 
     return copied_count
+
+
+
+
+
+## Drop all files in a folder
+def drop_all_files_in_a_folder(unc, chmod_value=0o777):
+    """
+    Deletes all files and subdirectories within a given directory.
+
+    Args:
+        unc: Path to the directory to be emptied.
+        chmod_value: Octal value for changing file/directory permissions before deletion. 
+                     Default is 0o777 (read, write, execute for all).
+
+    Raises:
+        OSError: If an error occurs during file or directory deletion.
+    """
+
+    for root, dirs, files in os.walk(unc, topdown=False):
+        for f in files:
+            file_path = os.path.join(root, f)
+            try:
+                os.chmod(file_path, chmod_value)
+                os.remove(file_path)
+            except OSError as e:
+                print(f"Error deleting file {file_path}: {e}")
+
+        for d in dirs:
+            dir_path = os.path.join(root, d)
+            try:
+                os.chmod(dir_path, chmod_value)
+                shutil.rmtree(dir_path)
+            except OSError as e:
+                print(f"Error deleting directory {dir_path}: {e}")
+
+if __name__=='__main__':
+    try:
+        drop_all_files_in_a_folder("path/to/your/folder") 
+    except OSError as e:
+        print(f"Error deleting files: {e}")
+
+
